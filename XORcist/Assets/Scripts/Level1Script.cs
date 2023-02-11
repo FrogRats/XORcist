66b38da98e6a1ea8b0c599b9f951115dd638f4c1
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2023-02-11 15:35:15 +0000

    AAAAA

---
+++ XORcist/Assets/Scripts/Level1Script.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Script : MonoBehaviour
+{
+    [Header("CRT Monitors")]
+    [SerializeField] GameObject CRTMonitor1;
+    [SerializeField] GameObject CRTMonitor2;
+
+
+    private int inputA = 1;
+    private int inputB = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CRTMonitor1.FUNCTIONHERE; 
+
+    }
+}
