2bd469e1d9580d2082f14dc2d8b1ff400e4c0521
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2023-02-12 01:48:53 +0000

    Level 4 - done

---
+++ XORcist/Assets/Scripts/Level4Script.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level4Script : MonoBehaviour
+{
+
+    [Header("CRT Monitors")]
+    [SerializeField] GameObject CRTMonitor1;
+    [SerializeField] GameObject CRTMonitor2;
+    [SerializeField] GameObject CRTMonitor3;
+    [SerializeField] GameObject CRTMonitor4;
+    [SerializeField] GameObject CRTMonitor5;
+    [SerializeField] GameObject CRTMonitor6;
+
+    [Header("Additional UI")]
+    [SerializeField] Button ritualButton;
+    [SerializeField] Image successFlame;
+
+    private CRTLogicScript CRTScript1;
+    private CRTLogicScript CRTScript2;
+    private CRTLogicScript CRTScript3;
+    private CRTLogicScript CRTScript4;
+    private CRTLogicScript CRTScript5;
+    private CRTLogicScript CRTScript6;
+
+    private bool inputA = true;
+    private bool inputB = true;
+    private bool inputC = true;
+    private bool inputD = true;
+
+    private bool currentStatus;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CRTScript1 = CRTMonitor1.GetComponent<CRTLogicScript>();
+        CRTScript2 = CRTMonitor2.GetComponent<CRTLogicScript>();
+        CRTScript3 = CRTMonitor3.GetComponent<CRTLogicScript>();
+        CRTScript4 = CRTMonitor4.GetComponent<CRTLogicScript>();
+        CRTScript5 = CRTMonitor5.GetComponent<CRTLogicScript>();
+        CRTScript6 = CRTMonitor6.GetComponent<CRTLogicScript>();
+
+        ritualButton.onClick.AddListener(CRTMonitorLogic);
+    }
+
+    private void CRTMonitorLogic()
+    {
+        // First CRT Monitor
+        currentStatus = CRTScript1.getCurrentGateOutput(inputA, inputB);
+
+        // Second CRT Monitor
+        currentStatus = CRTScript2.getCurrentGateOutput(inputC, currentStatus);
+
+        // Third CRT Monitor
+        currentStatus = CRTScript3.getCurrentGateOutput(inputD, !currentStatus);
+
+        // Fourth CRT Monitor
+        currentStatus = CRTScript4.getCurrentGateOutput(inputD, currentStatus);
+
+        // Fifth CRT Monitor
+        currentStatus = CRTScript5.getCurrentGateOutput(inputD, currentStatus);
+
+        // Sixth CRT Monitor
+        currentStatus = CRTScript6.getCurrentGateOutput(inputD, currentStatus);
+
+        if (currentStatus)
+        {
+            successFlame.gameObject.SetActive(true);
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
